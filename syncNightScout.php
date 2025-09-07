4531e9ff3da86aaa2dcef39488be884794044d88
Kevin Rider <github@thebackwaters.com>
2025-09-07 15:37:28 -0600

    consolidate and refactor

--- syncNightScout.php
+++ syncNightScout.php
@@ -9,196 +9,91 @@ $currentDate = $currentDate->modify('-1 week');
 $endDate = new DateTimeImmutable();
 $endDate = $endDate->modify('+1 day');
 
-syncEntries($currentDate, $endDate, $oldSecureDomain, $oldHash, $newSecureDomain, $hashedSecret);
-syncTreatments($currentDate, $endDate, $oldSecureDomain, $oldHash, $newSecureDomain, $hashedSecret);
-syncDeviceStatus($currentDate, $endDate, $oldSecureDomain, $oldHash, $newSecureDomain, $hashedSecret);
-syncProfile($oldSecureDomain, $oldHash, $newSecureDomain, $hashedSecret);
+syncEndpoint('entries', 'dateString', $currentDate, $endDate, $oldSecureDomain, $oldHash, $newSecureDomain, $hashedSecret);
+syncEndpoint('treatments', 'created_at', $currentDate, $endDate, $oldSecureDomain, $oldHash, $newSecureDomain, $hashedSecret);
+syncEndpoint('devicestatus', 'created_at', $currentDate, $endDate, $oldSecureDomain, $oldHash, $newSecureDomain, $hashedSecret);
+syncSingleton('profile', $oldSecureDomain, $oldHash, $newSecureDomain, $hashedSecret);
 
-function syncEntries(DateTimeImmutable $currentDate, DateTimeImmutable $endDate, string $oldSecureDomain, string $oldHash, string $newSecureDomain, string $hashedSecret): void {
-    while ($currentDate < $endDate) {
-        $loopFromDate = $currentDate->format('Y-m-d');
-        $currentDate = $currentDate->modify('+1 day');
-        $loopToDate = $currentDate->format('Y-m-d');
-
-        if (new DateTimeImmutable($loopToDate) > $endDate) {
-            $loopToDate = $endDate->format('Y-m-d');
-        }
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $oldSecureDomain .'/api/v1/entries.json?count=all&find[dateString][$lte]=' . $loopToDate . '&find[dateString][$gte]=' . $loopFromDate);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-        $headers = [];
-        $headers[] = 'api-secret: ' . $oldHash;
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-        $result = curl_exec($ch);
-        if (curl_errno($ch)) {
-            echo 'Error:' . curl_error($ch);
-        }
-        curl_close($ch);
-
-        $arr = json_decode($result, true);
-        if (!empty($arr)) {
-            $newArray = [];
-            foreach($arr as $item) {
-                unset($item['_id']);
-                $newArray[] = $item;
-            }
-            $newJSON = json_encode($newArray);
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $newSecureDomain . '/api/v1/entries');
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $newJSON);
-            $headers = array();
-            $headers[] = 'Content-Type: application/json';
-            $headers[] = 'api-secret: ' . $hashedSecret;
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            curl_exec($ch);
-            if (curl_errno($ch)) {
-                echo 'Error:' . curl_error($ch);
-            }
-            curl_close($ch);
-        }
-    }
-}
-
-function syncTreatments(DateTimeImmutable $currentDate, DateTimeImmutable $endDate, string $oldSecureDomain, string $oldHash, string $newSecureDomain, string $hashedSecret): void {
-    while ($currentDate < $endDate) {
-        $loopFromDate = $currentDate->format('Y-m-d');
-        $currentDate = $currentDate->modify('+1 day');
-        $loopToDate = $currentDate->format('Y-m-d');
-        if (new DateTimeImmutable($loopToDate) > $endDate) {
-            $loopToDate = $endDate->format('Y-m-d');
-        }
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $oldSecureDomain . '/api/v1/treatments.json?count=all&find[created_at][$lte]=' . $loopToDate . '&find[created_at][$gte]=' . $loopFromDate);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-        $headers = array();
-        $headers[] = 'api-secret: ' . $oldHash;
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        $result = curl_exec($ch);
-        if (curl_errno($ch)) {
-            echo 'Error:' . curl_error($ch);
-        }
-        curl_close($ch);
-
-        $arr = json_decode($result, true);
-        if (!empty($arr)) {
-            $newArray = [];
-            foreach ($arr as $item) {
-                unset($item['_id']);
-                $newArray[] = $item;
-            }
-            $newJSON = json_encode($newArray);
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $newSecureDomain . '/api/v1/treatments');
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $newJSON);
-
-            $headers = array();
-            $headers[] = 'Content-Type: application/json';
-            $headers[] = 'api-secret: ' . $hashedSecret;
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            curl_exec($ch);
-            if (curl_errno($ch)) {
-                echo 'Error:' . curl_error($ch);
-            }
-            curl_close($ch);
-        }
-    }
-}
-
-function syncDeviceStatus(DateTimeImmutable $currentDate, DateTimeImmutable $endDate, string $oldSecureDomain, string $oldHash, string $newSecureDomain, string $hashedSecret): void {
-    while ($currentDate < $endDate) {
-        $loopFromDate = $currentDate->format('Y-m-d');
-        $currentDate = $currentDate->modify('+1 day');
-        $loopToDate = $currentDate->format('Y-m-d');
-        if (new DateTimeImmutable($loopToDate) > $endDate) {
-            $loopToDate = $endDate->format('Y-m-d');
-        }
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $oldSecureDomain . '/api/v1/devicestatus.json?count=all&find[created_at][$lte]=' . $loopToDate . '&find[created_at][$gte]=' . $loopFromDate);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-        $headers = array();
-        $headers[] = 'api-secret: ' . $oldHash;
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        $result = curl_exec($ch);
-        if (curl_errno($ch)) {
-            echo 'Error:' . curl_error($ch);
-        }
-
-        curl_close($ch);
-        $arr = json_decode($result, true);
-        if (!empty($arr)) {
-            $newArray = [];
-            foreach ($arr as $item) {
-                unset($item['_id']);
-                $newArray[] = $item;
-            }
-
-            $newJSON = json_encode($newArray);
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $newSecureDomain . '/api/v1/devicestatus');
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $newJSON);
-            $headers = array();
-            $headers[] = 'Content-Type: application/json';
-            $headers[] = 'api-secret: ' . $hashedSecret;
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            curl_exec($ch);
-            if (curl_errno($ch)) {
-                echo 'Error:' . curl_error($ch);
-            }
-            curl_close($ch);
-        }
-    }
-}
-
-function syncProfile(string $oldSecureDomain, string $oldHash, string $newSecureDomain, string $hashedSecret): void {
+function _fetchFromNightscout(string $url, string $hash): ?array
+{
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $oldSecureDomain .'/api/v1/profile.json');
+    curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-    $headers = array();
-    $headers[] = 'api-secret: ' . $oldHash;
+    $headers = ['api-secret: ' . $hash];
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
     $result = curl_exec($ch);
     if (curl_errno($ch)) {
         echo 'Error:' . curl_error($ch);
+        curl_close($ch);
+        return null;
     }
     curl_close($ch);
+    return json_decode($result, true);
+}
 
-    $arr = json_decode($result);
-    if(!empty($arr->message) && $arr->message = 'Unauthorized') {
-        echo 'Error: ' . $arr->message;
-    }
-    $arr2 = json_decode($result, true);
-
+function _postToNightscout(string $url, string $hash, array $data): void
+{
     $newArray = [];
-    foreach($arr2 as $item) {
+    foreach ($data as $item) {
         unset($item['_id']);
         $newArray[] = $item;
     }
-
     $newJSON = json_encode($newArray);
+
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $newSecureDomain . '/api/v1/profile');
+    curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $newJSON);
-
-    $headers = [];
-    $headers[] = 'Content-Type: application/json';
-    $headers[] = 'api-secret: ' . $hashedSecret;
+    $headers = [
+        'Content-Type: application/json',
+        'api-secret: ' . $hash,
+    ];
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_exec($ch);
     if (curl_errno($ch)) {
         echo 'Error:' . curl_error($ch);
     }
     curl_close($ch);
+}
+
+function syncEndpoint(string $endpoint, string $dateField, DateTimeImmutable $currentDate, DateTimeImmutable $endDate, string $oldSecureDomain, string $oldHash, string $newSecureDomain, string $hashedSecret): void
+{
+    while ($currentDate < $endDate) {
+        $loopFromDate = $currentDate->format('Y-m-d');
+        $currentDate = $currentDate->modify('+1 day');
+        $loopToDate = $currentDate->format('Y-m-d');
+
+        if ($currentDate > $endDate) {
+            $loopToDate = $endDate->format('Y-m-d');
+        }
+
+        $url = sprintf(
+            '%s/api/v1/%s.json?count=all&find[%s][$lte]=%s&find[%s][$gte]=%s',
+            $oldSecureDomain,
+            $endpoint,
+            $dateField,
+            $loopToDate,
+            $dateField,
+            $loopFromDate
+        );
+
+        $data = _fetchFromNightscout($url, $oldHash);
+
+        if (!empty($data)) {
+            _postToNightscout($newSecureDomain . '/api/v1/' . $endpoint, $hashedSecret, $data);
+        }
+    }
+}
+
+function syncSingleton(string $endpoint, string $oldSecureDomain, string $oldHash, string $newSecureDomain, string $hashedSecret): void
+{
+    $url = sprintf('%s/api/v1/%s.json', $oldSecureDomain, $endpoint);
+    $data = _fetchFromNightscout($url, $oldHash);
+
+    if (!empty($data)) {
+        _postToNightscout($newSecureDomain . '/api/v1/' . $endpoint, $hashedSecret, $data);
+    }
 }
